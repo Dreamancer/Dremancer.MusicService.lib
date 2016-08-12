03cb9b8a26ca0d3886b35765f775ae5440a92765
Jakub Horniak <horniakj@gmail.com>
2016-08-12 23:05:23 +0200

    Add project files.

---
+++ Dremancer.MusicService.lib/MusicService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Dremancer.MusicService.lib
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+  public class MusicService : IMusicService
+  {
+    public Artist GetArtist(string artistId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Record> GetArtistRecords(string artisrtId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Record> GetRecord(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Record> GetShows(string artistId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool HasNewRecords(string artistId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<SimpleArtist> SearchArtists(string name)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
